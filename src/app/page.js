73b11c1856c96f12f6ff7be09d853c6e60a27423
Ritu Raj <145446035+riturajroman@users.noreply.github.com>
2025-06-06 14:23:57 +0530

    added president message

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Accommodation from "@/components/Accommodation";
 import AtmaConvention from "@/components/AtmaConvention";
 import Hero from "@/components/Hero";
+import President from "@/components/President";
 import RegistrationForm from "@/components/RegistrationForm";
 import SpecialHighlights from "@/components/SpecialHighlights";
 import WhyAttend from "@/components/WhyAttend";
@@ -10,6 +11,7 @@ export default function Home() {
     <div>
       <Hero />
       <section className="w-full h-25 bg-[#dc1d46]"></section>
+      <President />
       <AtmaConvention />
       <SpecialHighlights />
       <RegistrationForm />
